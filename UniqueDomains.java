9f95efae16fd2f45015e899a89f9c66b0243f911
mohitle <32513326+mohitle@users.noreply.github.com>
2017-10-16 02:51:38 +0200

    Update UniqueDomains.java
    Comments added

--- UniqueDomains.java
+++ UniqueDomains.java
@@ -14,18 +14,29 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+* @author Maria Grazia Lancellotti
+* @version 15 Oct 2017
+* This class allows to extract URLs contained in HTML source of a given URL.
+* The unique domains of the URLs are displayed as well as the number of occurrences of each domain.
+**/
 public class UniqueDomains {
 
+	/**
+	 * getAllLinks method reads from an URL chosen by the user to collect unique domains.
+	 * @return list of unique domains and number of occurrences.
+	 */
 	public static ArrayList<String> getAllLinks() {
 		ArrayList<String> finalList = new ArrayList<String>();
 		ArrayList<String> tempList = new ArrayList<String>();
 		String readString;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("please type");
+		System.out.println("Please type or paste URL: ");
 		String input = sc.next();
 		try {
 			URL url = new URL(input);
 			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			/* For each line of HTML file extract the text delimited by the tag <a></a> and add to the list to return. */
 			while ((readString = in.readLine()) != null) {
 				if (!readString.contains("<a") && !readString.contains("<link") && readString.contains("href=")) {
 					finalList.add(readString);
@@ -46,6 +57,7 @@ public class UniqueDomains {
 			}
 		}
 
+		/* Extract well-formed URL from the text within <a></a>. */
 		int counter = 0;
 		for (String u : finalList) {
 			String link = extractLink(u);
@@ -57,11 +69,17 @@ public class UniqueDomains {
 		
 		sc.close();
 
+		/* Need to record what is the URL input by user. */
 		finalList.add(input);
 		
 		return finalList;
 	}
 
+	/**
+	 * listUniqueDomains method extracts the unique domains from the list of URLs and counts the occurrences of each.
+	 * @param urls refers to the list of all the URLs contained in the HTML file.
+	 * @return list of unique domains as well as number of occurrences.
+	 */
 	public static HashMap<String, Integer> listUniqueDomains(ArrayList<String> urls) {
 		HashMap<String, Integer> domains = new HashMap<String, Integer>();
 
@@ -73,8 +91,11 @@ public class UniqueDomains {
 		String invalidUrls = "Invalid URLs";
 		int invalidUrlsCounter = 0;
 
-		int counter = 0;
+		/* For each URL keep the unique domain, store it and increase number of occurrence by 1.
+		 * If domain already exists, update occurrence.
+		 */
 		for (String u : urls) {
+			/* These 4 options are not computed as valid URLs. */
 			if (u.equals("#") || u.startsWith("javascript") || u.equals("") || u.equals(" ")) {
 				if (!domains.containsKey(invalidUrls)) {
 					domains.put(invalidUrls, invalidUrlsCounter + 1);
@@ -99,7 +120,6 @@ public class UniqueDomains {
 			}
 		}	
 
-
 		domains.put(mainUrlDomain, counterMainUrl);
 
 		System.out.println("Unique domains (" + domains.size() + ") - Number of links each");
@@ -111,6 +131,11 @@ public class UniqueDomains {
 		return domains;
 	}
 
+	/**
+	 * getDomain method extracts the domain from the URL.
+	 * @param url the URL to take the domain of.
+	 * @return String of the domain.
+	 */
 	public static String getDomain(String url) {
 		String uniqueDomain = "";
 		try {
@@ -122,6 +147,11 @@ public class UniqueDomains {
 		return uniqueDomain;
 	}
 
+	/**
+	 * extractTagAText method extracts text within <a></a> tags from (HTML) line.
+	 * @param text is the (HTML) line to extract from.
+	 * @return list with the sections extracted.
+	 */
 	public static ArrayList<String> extractTagAText(String text) {
 		ArrayList<String> listOfElemnts = new ArrayList<String>();
 		String regex = "(<a [^>]*href=)(.*?)(>|\\s|$)";
@@ -130,13 +160,16 @@ public class UniqueDomains {
 
 		while (matcher.find()) {
 			String element = matcher.group();
-			//			System.out.println(str);
 			listOfElemnts.add(element);
 		}
-		//		System.out.println("extract " + urls.toString());
 		return listOfElemnts;
 	}
-
+	
+	/**
+	 * extractLink method extract valid URL from tagged text. It is supposed to work after extractTagAText has taken text from HTML line.
+	 * @param text is the tagged text to extract the link from.
+	 * @return the link itself.
+	 */
 	public static String extractLink(String text) {
 		String link = "";
 		String[] partsOfLink = new String[2];
